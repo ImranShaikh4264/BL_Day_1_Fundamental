f495463337b85cc02bfeef6ea1d6587122e709fa
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-11-01 21:30:27 +0530

    Added: Get name using command line arguments

---
+++ NameUsingCommandLine.java
@@ -0,0 +1,7 @@
+package com.bridgelab.day1.fundamentals;
+
+public class NameUsingCommandLine {
+	public static void main(String args[]){  
+		System.out.println("Your first argument is: "+args[0]); 
+	}
+}
