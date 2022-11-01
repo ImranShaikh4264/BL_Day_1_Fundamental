7a3c146581f019341966a33cf267166966b61469
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-11-01 21:28:51 +0530

    Added: Find default value of primative datatypes

---
+++ DefaultValueOfPrimativeDataType.java
@@ -0,0 +1,24 @@
+package com.bridgelab.day1.fundamentals;
+
+public class DefaultValueOfPrimativeDataType {
+	static byte b;
+	static short s;
+	static int i;
+	static long l;
+	static float f;
+	static double d;
+	static char c;
+	static boolean bl;
+
+	public static void main(String[] args) {
+		System.out.println("The default values of Byte :" + b);
+		System.out.println("The default values of Short :" + s);
+		System.out.println("The default values of Int :" + i);
+		System.out.println("The default values of Long :" + l);
+		System.out.println("The default values of Float :" + f);
+		System.out.println("The default values of Double :" + d);
+		System.out.println("The default values of Char :" + c);
+		System.out.println("The default values of Boolean :" + bl);
+	}
+
+}
